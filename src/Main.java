ddd5503583ffa67bfe038acf6731c088d39ec873
MANIKANDAN-IT21 <manikandan.it21@bitsathy.ac.in>
2024-02-29 22:21:14 +0530

    all

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        //output
+//        System.out.println("hello Manikandan");
+//
+//        //input
+//        Scanner mani=new Scanner(System.in);
+//        System.out.print("enter the Number :");
+//        int madhan= mani.nextInt();
+//        System.out.println("madhan is a :"+ -madhan);
+//
+//        //array
+//        int box[]=new int [5];
+//        box[0]=12;
+//        box[1]=12;
+//        box[2]=12;
+//        box[4]=18;
+//        System.out.println(box.length);
+//        System.out.println(box[4]);
+//        int box1[]={1,2,3,44,5};
+//        System.out.println(box1[0]);
+//
+//        //2D-Array
+//        int box3[][]= new int [2][2]; // new is key word is used to create the new object or {1,2},{2,3}
+//        box3[0][0]=12;
+//        box3[0][1]=12;
+//        box3[1][0]=14;
+//        box3[1][1]=16;
+//        box3[1][1]=21;
+//        System.out.println(box3.length);
+//        System.out.println(box3[1][1]);
+//        String mani[]=new String[]{"mani","kandan","kumar","raj"};
+//        for(int i=0;i<mani.length;i++)
+//        {
+//            System.out.print(mani[i]);
+//
+//        }
+
+//        int[] mani; // declaration the array
+//        mani=new int[5];// initialization of the array and new keyword is created the object in heap memory
+        String[] mani=new String[4];
+        mani[2]="manikandan";
+        mani[3]="kandan";
+        mani[2]="manikamaraj";
+        System.out.println(mani[10]);
+
+
+
+    }
+}
