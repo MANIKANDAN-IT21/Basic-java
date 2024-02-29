ddff3f7013281b5200b4d4449e993f151201293a
MANIKANDAN-IT21 <manikandan.it21@bitsathy.ac.in>
2024-02-29 22:33:49 +0530

    first

---
+++ src/Array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ class Array {
+     public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        int[] mani= new int[5];
+        for (int i=0;i < mani.length;i++);
+         {
+             mani[i]=input.nextInt();
+
+         }
+     }
+ }
