38b66d52b4af7e296d2af5deabbe5217e136d8a0
MANIKANDAN-IT21 <manikandan.it21@bitsathy.ac.in>
2024-02-29 22:43:37 +0530

    array last

--- src/Array.java
+++ src/Array.java
@@ -1,12 +1,20 @@
 import java.util.*;
  class Array {
+     private static int i;
+
      public static void main(String[] args) {
 
         Scanner input=new Scanner(System.in);
         int[] mani= new int[5];
         for (int i=0;i < mani.length;i++);
          {
+             mani[i]=input.nextInt();
 
+         }
+         for (int i=0;i < mani.length;i++);
+         {
+
+             System.out.println(mani[i]);
          }
      }
  }
